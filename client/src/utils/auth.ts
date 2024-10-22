2679f3c80b6d82bea5a7f1328eb038cc819772a3
ventell virgo <lennyasme@gmail.com>
2024-10-22 13:13:48 -0400

    updated authAPIs in client, index.ts and auth.ts

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,45 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return jwtDecode<JwtPayload>(token);
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    if (!token) {
+      return false;
+    }
+    return !this.isTokenExpired(token);
+
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+   const payload = jwtDecode<JwtPayload>(token);
+    return Date.now() >= payload.exp * 1000;
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';  
+    return loggedUser;
   }
+  
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/login');
   }
 }
 
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,20 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user as JwtPayload;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
 };
